08b17f4f577cbe33989d4e33ddc15e269fc8f982
renxzen <renxzen@gmail.com>
2023-09-09 22:52:46 -0500

    added simple html server in elysia

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
+
+const app = new Elysia()
+	.use(html())
+	.get("/", ({ html }) => html(baseHtml))
+	.listen(3000);
+
+console.log(
+	`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
+);
+
+const baseHtml = `
+	<!doctype html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8" />
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1"
+			/>
+			<title>THE BETH STACK</title>
+		</head>
+		<body>
+			I'm a html document
+		</body>
+	</html>
+`;
